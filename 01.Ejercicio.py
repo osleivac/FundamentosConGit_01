8fef6a0cb9e507c6acfefa8e770ed4aa888fa655
ignacio <ig.sariego@duocuc.cl>
2025-06-22 18:48:02 -0400

    Segundo commit. Codigo actualizado para pedir cantidad de notas y guardarlas en una lista.

--- 01.Ejercicio.py
+++ 01.Ejercicio.py
@@ -1,10 +1,31 @@
-def leer_nota(dato):
+def pedir_cantidad_notas():
     while True:
         try:
-            entrada = float(input(dato))
-            if entrada >= 1.0 and entrada <= 7.0:
-                return entrada
+            cantidad = int(input("Ingresa la cantidad de notas: "))
+            if cantidad > 1:
+                break
+            else:
+                print("Debes ingresar al menos una nota.")
+        except:
+            print("Valor no valido. Solo puedes ingresar numeros.")
+    return cantidad
+
+cantidad = pedir_cantidad_notas()
+
+def leer_nota():
+    notas = []
+    contador = 0
+    while contador < cantidad:
+        try:
+            nota = float(input("Ingresa tus notas: "))
+            if nota >= 1 and nota <= 7:
+                notas.append(nota)
+                contador += 1
             else:
                 print('Debe ingresar una nota entre 1 y 7')
         except:
             print('Valor no válido. Intente nuevamente.')
+    return notas
+
+notas = leer_nota()
+print(notas)
